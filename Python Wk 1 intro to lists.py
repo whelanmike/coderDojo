2de97d3888072ba0b5536baae02f66aeda88f451
Mike Whelan <whelanmike@gmail.com>
2019-01-30 16:10:54 +0000

    Create Python Wk 1 intro to lists.py

---
+++ Python Wk 1 intro to lists.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 16:07:49 2019
+
+@author: mike.whelan
+"""
+# get the sum of the first 10 numbers 
+# first try
+i = 0       # start at 0
+total = i   # variable to keep running total
+while i < 10:
+    i+= 1
+    print (i)
+    total += i
+print (total)    
+
+
+#L = [i for i in range(11)]
+#dir (L)
+

2802908441abd826c20c40d4a313c9d8ddf6d228
Mike Whelan <whelanmike@gmail.com>
2019-01-30 16:22:51 +0000

    Update Python Wk 1 intro to lists.py

--- Python Wk 1 intro to lists.py
+++ Python Wk 1 intro to lists.py
@@ -2,19 +2,26 @@
 """
 Created on Wed Jan 30 16:07:49 2019
 
-@author: mike.whelan
+@author: 
 """
-# get the sum of the first 10 numbers 
+# get the sum and average of the first 10 numbers 
 # first try
-i = 0       # start at 0
-total = i   # variable to keep running total
+print ('First try getting sum & avg')
+i = 1           # start at 0
+total = i       # variable to keep running total
 while i < 10:
     i+= 1
     print (i)
     total += i
-print (total)    
+
+print ('total is   ', total)
+print ('average is ', total / i)
 
 
-#L = [i for i in range(11)]
-#dir (L)
 
+# second try -- use a List
+print ('==================================================')
+print ('Second try getting sum & avg')
+# create a list of the 10 numbers
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print ('myList contains these elements :- ',  myList )

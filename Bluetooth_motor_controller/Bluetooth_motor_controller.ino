04c6ce7bdb8ecd458a60fdaed7ef97ecd84073e8
Mike Whelan <whelanmike@gmail.com>
2019-02-25 12:53:23 +0000

    Update Bluetooth_motor_controller.ino
    Make code more modular.
    Remove unused variables.

--- Bluetooth_motor_controller/Bluetooth_motor_controller.ino
+++ Bluetooth_motor_controller/Bluetooth_motor_controller.ino
@@ -1,9 +1,5 @@
 #include <SoftwareSerial.h>
-// BLE-4 module config (HM-10)
-//int Tx 8;
-//int Rx 9;
-SoftwareSerial mySerial(8, 9); // Tx, Rx
-String bleString ;
+SoftwareSerial bluetoothSerial(8, 9); // Tx, Rx
 
 //int RELAY_1 = 7; //Declaring the pins where in1 in2 from the driver are wired
 //int RELAY_2 = 3;
@@ -13,34 +9,29 @@ int RPWM_output = 5;
 int LPWM_output = 6;
 int led = 13 ;
 int duration = 7000;
-unsigned long time_now = 0;
-int voltage = 00;
-int stepper = 0;
+unsigned long time_start = 0;
+int voltage = 0;
 unsigned long elapsed = 0;
 char dirn = 'A';
 char c = ' ';
 
 void setup() {
   Serial.begin(115200);
-//  pinMode(RELAY_1, OUTPUT); //Declaring the pin modes, obviously they're outputs
-//  pinMode(RELAY_2, OUTPUT);
   pinMode(RELAY_3, OUTPUT);
   pinMode(RELAY_4, OUTPUT);
   pinMode(RPWM_output, OUTPUT);
   pinMode(LPWM_output, OUTPUT);
   pinMode(led, OUTPUT);
   digitalWrite(13, LOW);
-//  digitalWrite(RELAY_1, HIGH);  // OFF
-//  digitalWrite(RELAY_2, HIGH);  // OFF
   digitalWrite(RELAY_3, HIGH);  // OFF
   digitalWrite(RELAY_4, HIGH);  // OFF
-  mySerial.begin(9600);
+  bluetoothSerial.begin(115200);
   delay(100);
 }
 
 void loop(){
-  if (mySerial.available()>0){
-    c = mySerial.read();
+  if (bluetoothSerial.available()>0){
+    c = bluetoothSerial.read();
     Serial.write(c);
   }
   if (c == 'X'){
@@ -51,23 +42,17 @@ void loop(){
     }
 
   if (c == 'F' && dirn != 'F' && dirn != 'R'){
-    time_now = millis();
-    stepper = 0;
-    voltage = 0;
-    dirn = 'F';
+    initVars();
     analogWrite (RPWM_output, 0);
     }
   if (c == 'R' && dirn != 'R' && dirn != 'F'){
-    time_now = millis();
-    stepper = 0;
-    voltage = 0;
-    dirn = 'R';
+    initVars();
     analogWrite (LPWM_output, 0);
     }
-    
-  elapsed = millis() - time_now;
-  
-  if (dirn == 'F'){     // Forward Direction  
+
+  elapsed = millis() - time_start;
+
+  if (dirn == 'F'){     // Forward Direction
     startMotor ('F', LPWM_output);
   }
 
@@ -75,7 +60,7 @@ void loop(){
     startMotor ('R', RPWM_output);
   }
 }
- 
+
 void PowerOn(){
   digitalWrite(RELAY_3, LOW);
   digitalWrite(RELAY_4, LOW);
@@ -85,29 +70,25 @@ void PowerOff(){
   digitalWrite(RELAY_3, HIGH);
   digitalWrite(RELAY_4, HIGH);
 }
+void initVars(){
+  time_start = millis();
+  voltage = 0;
+  dirn = c;
+}
 void startMotor (char dirn, int PWM_dirn){
   if (elapsed < duration){
     analogWrite (PWM_dirn, voltage);
-    if (stepper <= 2000){
       voltage += 1;
       if ( voltage > 254 ){
         voltage = 255;
       }
-      stepper += 1;
-    }
   }
-  if (millis() - time_now >= duration){
+  if (elapsed >= duration){
     voltage -= 1;
     analogWrite (PWM_dirn, voltage);
-    if ( stepper > 0 ){
-      stepper -= 1;
       voltage -= 1;
-      if ( voltage < 0){
-        stepper = 0;
+      if ( voltage <= 0){
+        dirn = 'A'; // Flag STOP
       }
-    }
-    if (stepper <= 0){
-      dirn = 'A';   // Flag Stopped.
-    }
   }
 }

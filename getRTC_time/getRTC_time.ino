852843b5dfd9ad372a77ba1ebf1ec3068e7a6c5c
whelanmike <whelanmike@users.noreply.github.com>
2019-06-18 22:45:31 +0100

    Update getRTC_time.ino
    Use RTC Interrupt to wake up uC.

--- getRTC_time/getRTC_time.ino
+++ getRTC_time/getRTC_time.ino
@@ -1,6 +1,9 @@
+#include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
 #include <DS3232RTC.h>
 #include <SPI.h>
 #include <SD.h>
+
+#define interruptPin 2 //Pin we are going to use to wake up the Arduino
 
 const int chipSelect = 6;
 char charFileName[16] = "";
@@ -11,6 +14,8 @@ File logFile;
 float temperature_reading;
 String temperature_str;
 String logTime;
+const int time_interval = 5;// Sets the wakeup intervall in minutes
+
 void setup()
 {
     Serial.begin(9600);        // Starts serial communication in 9600 baud rate.
@@ -29,6 +34,29 @@ void setup()
     while (1);
   }
   Serial.println("initialization done.");
+
+  pinMode(LED_BUILTIN,OUTPUT);//We use the led on pin 13 to indecate when Arduino is A sleep
+  pinMode(interruptPin,INPUT_PULLUP);//Set pin d2 to input using the buildin pullup resistor
+  digitalWrite(LED_BUILTIN,HIGH);//turning LED on
+
+  RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
+  RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
+  RTC.alarm(ALARM_1);
+  RTC.alarm(ALARM_2);
+  RTC.alarmInterrupt(ALARM_1, false);
+  RTC.alarmInterrupt(ALARM_2, false);
+  RTC.squareWave(SQWAVE_NONE);
+  
+  time_t t; //create a temporary time variable so we can set the time and read the time from the RTC
+  t=RTC.get();//Gets the current time of the RTC
+  RTC.setAlarm(ALM1_MATCH_MINUTES , 0, minute(t)+time_interval, 0, 0);// Setting alarm 1 to go off 5 minutes from now
+  // clear the alarm flag
+  RTC.alarm(ALARM_1);
+  // configure the INT/SQW pin for "interrupt" operation (disable square wave output)
+  RTC.squareWave(SQWAVE_NONE);
+  // enable interrupt output for Alarm 1
+  RTC.alarmInterrupt(ALARM_1, true);
+
 }
 
 void loop()
@@ -48,8 +76,8 @@ void loop()
 //    Serial.println (displayTime());
     logFile = SD.open(charFileName, FILE_WRITE);
     if ( logFile ){
-      logFile.println( logString );
-//      println (currentTimeStamp);
+      logFile.println (logString);
+      Serial.println (logString);
       logFile.close();
     }
   // if the file isn't open, pop up an error:
@@ -57,7 +85,9 @@ void loop()
     Serial.println("error opening file :- " + fileName);
   }
     
-    delay(60000);       // 1 second interval between prints.
+    delay(5000);       // 1 minute interval between prints.
+    Going_To_Sleep();
+
 }
 
 String displayTime()  // Function that prints the time to serial monitor.
@@ -109,4 +139,33 @@ String getFileName()  // Function get  filename for today.
 //    Serial.println (minute());
 //    Serial.println (second());
     return filename;
+}
+
+void Going_To_Sleep(){
+    sleep_enable();//Enabling sleep mode
+    attachInterrupt(0, wakeUp, LOW);//attaching a interrupt to pin d2
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sleep
+    digitalWrite(LED_BUILTIN,LOW);//turning LED off
+    time_t t;// creates temp time variable
+    t=RTC.get(); //gets current time from rtc
+    Serial.println("Sleep  Time: "+String(hour(t))+":"+String(minute(t))+":"+String(second(t)));//prints time stamp on serial monitor
+    delay(1000); //wait a second to allow the led to be turned off before going to sleep
+    sleep_cpu();//activating sleep mode
+    Serial.println("just woke up!");//next line of code executed after the interrupt 
+    digitalWrite(LED_BUILTIN,HIGH);//turning LED on
+//    temp_Humi();//function that reads the temp and the humidity
+    t=RTC.get();
+    Serial.println("WakeUp Time: "+String(hour(t))+":"+String(minute(t))+":"+String(second(t)));//Prints time stamp 
+    //Set New Alarm
+    RTC.setAlarm(ALM1_MATCH_MINUTES , 0, minute(t)+time_interval, 0, 0);
+  
+  // clear the alarm flag
+  RTC.alarm(ALARM_1);
+  }
+
+void wakeUp(){
+  Serial.println("Interrrupt Fired");//Print message to serial monitor
+   sleep_disable();//Disable sleep mode
+  detachInterrupt(0); //Removes the interrupt from pin 2;
+ 
 }

992a24fdfc93ebeca85c8f8e48c7fb4e6b7f7e2e
ext.mike.whelan <ext.mike.whelan@cloudsphere.com>
2022-05-31 09:58:20 +0100

    Working Code for protptype hardware

--- solarPumpController/solarPumpController.ino
+++ solarPumpController/solarPumpController.ino
@@ -2,8 +2,8 @@
 #include <DS3232RTC.h>
 #include <SD.h>
 
-//#include "Wire.h"
-//#define DS3232_I2C_ADDRESS 0x68
+#include "Wire.h"
+#define DS3232_I2C_ADDRESS 0x68
 
 #define interruptPin 2 //Pin we are going to use to wake up the Arduino  interrupt pin (D2)
 #define NOP __asm__("nop\n\t")
@@ -15,103 +15,130 @@ File               logFile;
 float              temperature_float  = 0.0;
 char               temperature_char[10];
 String             logTime;
-const byte         sleep_for_mins     = 1; // Sets the wakeup intervall in minutes
+const byte         sleep_for_mins     = 1; // Sets the wake up intervall in minutes
 unsigned int       Year, Month, Day, Hour, Minute, Second;  // Function to update these values.
 boolean            isPumping          = false;
 const unsigned int repeatCheckMinutes = 1;
 unsigned long      currentTime        = 0;
 unsigned long      lastCheckTime      = 0 ;
-unsigned int       sleepTimeMinutes   = 15;
+unsigned int       sleepTimeSeconds   = 30; //* 60; //15;
 unsigned int       currentPressure    = 0;
 unsigned int       previousPressure   = 0;
-const unsigned int pressureLow        = 10;
-const unsigned int pressureHigh       = 300;
-const unsigned int pressureDelta      = 3;  // Indicates that tank is filling.
+const unsigned int pressureLow        = 230;
+const unsigned int pressureHigh       = 350;
+const unsigned int pressureDelta      = 0;  // Indicates that tank is filling.
 unsigned int       batteryVoltage;
 unsigned int       panelVoltage;
-const unsigned int relayPin           = 0; //TBD
+const unsigned int relayPin           = 8; //D8 pin -- connected to relay.
+time_t t;
+
+DS3232RTC RTC;
 
 void setup()
 {
 //    Wire.begin();   // REM
-    Serial.begin(9600);        // Starts serial communication in 9600 baud rate.
+    Serial.begin(19200);        // Starts serial communication in 9600 baud rate.
+    RTC.begin();
     detachInterrupt(0);    
     setSyncProvider(RTC.get);  // Library function to get the time from the RTC module.
     if (timeStatus() != timeSet)
-      Serial.println("System Time Cannot be Set. Check Connections.");
+      {
+        Serial.println("System Time Cannot be Set. Check Connections.");
+        Serial.println(timeStatus());
+        Serial.println(RTC.get()+7UL*3600UL);
+      }
     else
+    {
       Serial.println("System Time is Set.");
-    
-    Serial.print("Initializing SD card...");
+      Serial.println(RTC.get()+7UL*3600UL);
+    }
+    displayAlarm1();
+    Serial.println("Initializing SD card...");
     
     // see if the card is present and can be initialized:
     if (!SD.begin(sd_chipSelect)) {
       Serial.println("initialization failed!");
       while (1);
     }
+    else{
+      Serial.println("SD Card OK");
+    }
+
     Serial.println("initialization done.");
     
     pinMode(LED_BUILTIN, OUTPUT);         //We use the led on pin 13 to indicate when Arduino is asleep
+    pinMode(relayPin, OUTPUT);
+    digitalWrite(relayPin, HIGH);
     pinMode(interruptPin, INPUT_PULLUP);  //Set pin d2 to input using the builtin pullup resistor
     digitalWrite(LED_BUILTIN, HIGH);      //turning LED on
 
-    RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
-    RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
-    RTC.alarm(ALARM_1);
-    RTC.alarm(ALARM_2);
-    RTC.alarmInterrupt(ALARM_1, false);
-    RTC.alarmInterrupt(ALARM_2, false);
-    RTC.squareWave(SQWAVE_NONE);          // configure the INT/SQW pin for "interrupt" operation (disable square wave output)
+    RTC.setAlarm(DS3232RTC::ALM1_MATCH_DATE, 0, 0, 0, 1);
+    RTC.setAlarm(DS3232RTC::ALM2_MATCH_DATE, 0, 0, 0, 1);
+    RTC.alarm(DS3232RTC::ALARM_1);
+    RTC.alarm(DS3232RTC::ALARM_2);
+    RTC.alarmInterrupt(DS3232RTC::ALARM_1, false);
+    RTC.alarmInterrupt(DS3232RTC::ALARM_2, false);
+    RTC.squareWave(DS3232RTC::SQWAVE_NONE);          // configure the INT/SQW pin for "interrupt" operation (disable square wave output)
     
     time_t t;                             //create a temporary time variable so we can set the time and read the time from the RTC
     t = RTC.get();                        //Gets the current time of the RTC
-    RTC.setAlarm(ALM1_MATCH_MINUTES , 0, ((minute(t) + sleep_for_mins)%60)+00001, 0, 0); // Setting alarm 1 to go off X minutes from now. (Denis Barrett Fix!! using MOD)
+    RTC.setAlarm(DS3232RTC::ALM1_MATCH_SECONDS, (second(t) + sleepTimeSeconds) % 60, 0, 0, 0); //  USE for testing TRIGGER every few seconds.
+//    RTC.setAlarm(DS3232RTC::ALM1_MATCH_MINUTES , 0, ((minute(t) + sleep_for_mins)%60)+00001, 0, 0); // Setting alarm 1 to go off X minutes from now. (Denis Barrett Fix!! using MOD) PROD ***********
 //    Serial.print ((minute(t) + sleep_for_mins) % 60)+1; Serial.println(" <==== Alarm Minute First setting");
-    RTC.alarm(ALARM_1);                   // clear the alarm flag
-    RTC.squareWave(SQWAVE_NONE);
-    RTC.alarmInterrupt(ALARM_1, true);    // enable interrupt output for Alarm 1
-//       displayAlarm1();
+    RTC.alarm(DS3232RTC::ALARM_1);                   // clear the alarm flag
+    RTC.squareWave(DS3232RTC::SQWAVE_NONE);
+    RTC.alarmInterrupt(DS3232RTC::ALARM_1, true);    // enable interrupt output for Alarm 1
+      //  displayAlarm1();
 //   displayAlarm2();
-
+    Serial.println('End of setup()');
 }
 
 void loop()
 {
     if (! isPumping){
+        // Serial.println("isPumping   -------<NO>");
         updateTime();
-//        Serial.println (String(Year) + "--" +String(Month) +"--" + String(Day) );
+        // Serial.println (String(Year) + "--" +String(Month) +"--" + String(Day) +"--" + String(Hour) +"--" + String(Minute) +"--" + String(Second));
         sprintf (charFileName, "%04d%02d%02d.log", Year, Month, Day);
-        Serial.print (String (charFileName)); Serial.println( "                   | file name |     ");
+        // Serial.print (String (charFileName)); Serial.println( "                   | file name |     ");
         temperature_float = RTC.temperature() / 4.0;
+        // Serial.println ("Current Temperature : " + String(temperature_float));
         dtostrf (temperature_float, 4, 2, temperature_char);  // https://stackoverflow.com/questions/27651012/arduino-sprintf-float-not-formatting
-        if (aMinuteSinceLastReading){
-            currentPressure = readPressure();
-//            Serial.print ("Pressure Reading : "); Serial.println (String(currentPressure));
-            if (currentPressure < pressureLow){
-                Serial.println ("LOW Pressure. Set isPumping ON ");
-                setPumping (true);
-            }
-          }
+        currentPressure = readPressure();
+        Serial.print("Current Pressure NOT Pumping :: "); Serial.println(currentPressure);
+        // Simulate Logging to SD Card
         logString = (displayTime());
+        // Serial.println ("Current Time : " + logString);
         logString += "|" + String(temperature_char) +"|";
         logString += String (currentPressure) + "|";
         logFile = SD.open(charFileName, FILE_WRITE);   // This has to be a character array.
         if ( logFile ) {
           logFile.println (logString);
-          Serial.print (logString); Serial.println (" | log data  | ");
+          // Serial.print("SD Card write Simulation.   "); Serial.print (logString); Serial.println (" | log data  | ");
           logFile.close();
         }
         else {                            // if the file isn't open, pop up an error:
           Serial.println("error opening file :- " + String(charFileName));
         }
-//        previousPressure = currentPressure;
-//        lastCheckTime = millis();
-        delay(5000);       // 5 sec delay
-        Going_To_Sleep();
+
+        if (currentPressure < pressureLow){
+            Serial.println ("LOW Pressure. Set isPumping ON ");
+            setPumping (true);
+            }
+        else{
+          // }
+            delay(1000);       // 5 sec delay  **** GET RID OF THIS
+            t = RTC.get();
+            Going_To_Sleep();
+        }
       }
     else{
+        updateTime();
+        // Serial.println("isPumping   -------<YES>");
         if (aMinuteSinceLastReading){
-            currentPressure = readPressure();
+            currentPressure = readPressure(); 
+            Serial.println(currentPressure);
+            // Serial.print("Current Pressure during Pumping :: "); Serial.println(currentPressure);
             if (currentPressure > pressureHigh){
                 Serial.println ("HIGH Pressure. Tank full. Set isPumping OFF ");
                 setPumping (false);
@@ -129,14 +156,17 @@ void loop()
         else{                             // if a reading has been taken recently, do nothing.
             NOP;
         }
-    }
+    delay(500);
     lastCheckTime = millis();
+    }
+    // Serial.println("End of main() loop,");
+    // previousPressure = currentPressure;
 }
-
 /* END loop . Define functions after this */
+
 void updateTime()                         // Function that prints the time to serial monitor.
 {
-    time_t t;
+    // time_t t;
     t = RTC.get();                        // Get the current time.
     Year = year(t);                       // Update the following GLOBAL vars
     Month = month(t);
@@ -153,35 +183,42 @@ String displayTime(){                     // return timestamp (YYYY-MM-DD HH:MM:
 }
 
 void Going_To_Sleep() {
-    sleep_enable();                       //Enabling sleep mode
-    attachInterrupt(0, wakeUp, LOW);      //attaching a interrupt to pin d2
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);  //Setting the sleep mode, in our case full sleep
-    digitalWrite(LED_BUILTIN, LOW);       //turning LED off
-    time_t t;                             // creates temp time variable
+    Serial.println("Going to Sleep function()");
     t = RTC.get();                        //gets current time from rtc
 //    Serial.println("Sleep  Time: " + String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t))); //prints time stamp on serial monitor
+    RTC.alarm(DS3232RTC::ALARM_1);                   // clear the alarm flag
+    RTC.setAlarm(DS3232RTC::ALM1_MATCH_SECONDS, (second(t) + sleepTimeSeconds) % 60, 0, 0, 0); //  USE for testing TRIGGER every few seconds.
+    // Serial.print("---------------------------------------------------------seconds alarm matching-> ");
+    // Serial.println ((second(t) + 10) % 60);
+//    Serial.print ((minute(t) + sleep_for_mins) % 60); Serial.println(" <==== Alarm Minute setting");
+    // RTC.squareWave(DS3232RTC::SQWAVE_NONE);
+    RTC.alarmInterrupt(DS3232RTC::ALARM_1, true);    // enable interrupt output for Alarm 1
+    sleep_enable();                       //Enabling sleep mode
+    attachInterrupt(0, wakeUp, LOW);      //attaching an interrupt to pin d2
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);  //Setting the sleep mode, in our case full sleep
+    digitalWrite(LED_BUILTIN, LOW);       //turning LED off
+    // time_t t;                             // creates temp time variable
+
+  // Serial.print ("Alarm_1 time2 ====> ");
+  // displayAlarm1();
+
     delay(1000);                          //wait a second to allow the led to be turned off before going to sleep
+    // sei();
     sleep_cpu();                          //activating sleep mode
-//    Serial.println("just woke up!");      //next line of code executed after the interrupt
+
+    // Serial.println("just woke up!");      //next line of code executed after the interrupt
     digitalWrite(LED_BUILTIN, HIGH);      //turning LED on
   
-    t = RTC.get();
-//    Serial.println("WakeUp Time: " + String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t))); //Prints time stamp
+//    Serial.println("Wake Up Time: " + String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t))); //Prints time stamp
   
     //Set New Alarm
-    RTC.setAlarm(ALM1_MATCH_MINUTES , 21, ((minute(t) + sleep_for_mins) % 60), 21, 0);
-//    Serial.print ((minute(t) + sleep_for_mins) % 60); Serial.println(" <==== Alarm Minute setting");
-    RTC.alarm(ALARM_1);                   // clear the alarm flag
-    RTC.squareWave(SQWAVE_NONE);
-    RTC.alarmInterrupt(ALARM_1, true);    // enable interrupt output for Alarm 1
-
-//   displayAlarm1();
+//    RTC.setAlarm(DS3232RTC::ALM1_MATCH_MINUTES , 21, ((minute(t) + sleep_for_mins) % 60), 21, 0); // PROD ALARM_1 config
 //   displayAlarm2();
     
 }
 
 void wakeUp() {
-//    Serial.println("Interrrupt Fired");   //Print message to serial monitor
+    Serial.println("Wake Up function() [Interrupt]");   //Print message to serial monitor
     sleep_disable();                      //Disable sleep mode
     detachInterrupt(0);                   //Removes the interrupt from pin 2;
 }
@@ -189,10 +226,10 @@ void wakeUp() {
 void setPumping (boolean onOff){
     isPumping = onOff;
     if (isPumping){
-        digitalWrite (relayPin, HIGH);
+        digitalWrite (relayPin, LOW);
     }
     else{
-        digitalWrite (relayPin, LOW);
+        digitalWrite (relayPin, HIGH);
     }
 }
 
@@ -207,6 +244,10 @@ boolean aMinuteSinceLastReading (){
 }
 
 unsigned int readPressure(){              // Pressure Sensor on A0
+    if (second(t) % 5 ==0 ){              // This is just for debugging REMOVE in PROD
+    Serial.println("Read Pressure function()"); 
+    }
+    delay(100);
     return analogRead (A0);
 }
 
@@ -217,10 +258,12 @@ byte decToBcd(byte val)
   return ( (val/10*16) + (val%10) );
 }
 
+*/
 // Convert binary coded decimal to normal decimal numbers
 byte bcdToDec(byte val)
 {
-  return ( (val/16*10) + (val%16) );
+  // return ( (val/16*10) + (val%16) );
+  return ( (val >> 4) * 10 + (val & 0x0F) );
 }
 
 void readDS3232alarm1(byte *second, 
@@ -238,7 +281,7 @@ byte *dayOfMonth)
   *hour       = bcdToDec(Wire.read() & 0x3f);
   *dayOfMonth = bcdToDec(Wire.read()) ;
 }
-
+/*
 void readDS3232alarm2(
 byte *minute, 
 byte *hour, 
@@ -276,9 +319,9 @@ void setDS3232alarm2(byte minute, byte hour, byte dayOfMonth)
   Wire.write(decToBcd(dayOfMonth));  // set date of month
   Wire.endTransmission();
 }
+*/
 
-void displayAlarm1()
-{
+void displayAlarm1(){
   byte second, minute, hour, dayOfMonth;
   
   // retrieve data from DS3232  
@@ -286,23 +329,23 @@ void displayAlarm1()
   
   // send it to the serial monitor
   Serial.print("Alarm One: ");
-  Serial.print(hour, DEC);// convert the byte variable to a decimal number when being displayed
+  Serial.print(hour);// convert the byte variable to a decimal number when being displayed
   Serial.print(":");
   if (minute<10)
   {
       Serial.print("0");
   }
-  Serial.print(minute, DEC);
+  Serial.print(minute);
   Serial.print(":");
   if (second<10)
   {
       Serial.print("0");
   }
-  Serial.print(second, DEC);
-  Serial.print("  ");
-  Serial.println(dayOfMonth, DEC);
+  Serial.print(second);
+  Serial.print(" == ");
+  Serial.println(dayOfMonth);
 }
-
+/*
 void displayAlarm2()
 {
   byte minute, hour, dayOfMonth;

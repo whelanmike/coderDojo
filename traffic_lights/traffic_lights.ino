68db85dc3b4890ca6741dd9202690c059a5f943d
whelanmike <whelanmike@users.noreply.github.com>
2022-11-20 14:35:58 +0000

    Update traffic_lights.ino
    Added Comments to code.

--- traffic_lights/traffic_lights.ino
+++ traffic_lights/traffic_lights.ino
@@ -1,14 +1,14 @@
-int GREEN = 2;
-int YELLOW = 3;
-int RED = 4;
-int buttonPin = 5;
-int buttonState = 0;
+int GREEN = 2;        // Wire Pin 2 to Green LED
+int YELLOW = 3;       // Pin 3 to Yellow LED
+int RED = 4;          // Pin 4 to Red LED
+int buttonPin = 5;    // Button to detect pedestrian crossing.
+int buttonState = 0;  // variable to tell if button has been pressed on not. 
 
 void setup() {  // put your setup code here, to run once:
-  pinMode(GREEN, OUTPUT);
+  pinMode(GREEN, OUTPUT);         // All LEDs are output.  We send power out to turn them on.
   pinMode(YELLOW, OUTPUT);
   pinMode(RED, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT);      // Pedestrian button is read in by the controller.
 }
 
 void loop() {// put your main code here, to run repeatedly:
@@ -35,15 +35,16 @@ void loop() {// put your main code here, to run repeatedly:
      
 }
 
-  void flashLights(){
-    digitalWrite(GREEN, HIGH);
-    delay(100);      
-    digitalWrite(YELLOW, HIGH);
-    digitalWrite(GREEN, LOW);
-    delay(100);      
-    digitalWrite(RED, HIGH);
-    digitalWrite(YELLOW, LOW);
-    delay(100);      
-    digitalWrite(RED, LOW);
-    delay(10);
-  } 
+
+void flashLights(){
+  digitalWrite(GREEN, HIGH);
+  delay(100);      
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(GREEN, LOW);
+  delay(100);      
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, LOW);
+  delay(100);      
+  digitalWrite(RED, LOW);
+  delay(10);
+} 
